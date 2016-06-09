1d32e748694436f3ce363ac8da4313e97801453e
billCaplan <bill.j.caplan@gmail.com>
2016-06-08 17:25:59 -0700

    Limited infection is faster, and rendered differently

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,19 +1,37 @@
 module StaticPagesHelper
-  def limited_infection(desired_number)
+    def limited_infection(desired_number)
 
-    valid_options = []
-    @users.each do |student|
-      result = student.total_infection
-      if (result[0] === desired_number)
-        result[4] = student
-        valid_options.push(result)
+      seen_students = Hash.new("no")
+
+      valid_options = []
+
+      @users.each do |student|
+
+        if seen_students[student] === "yes"
+          next
+        else
+          result = student.total_infection
+          if (result[0] === desired_number)
+            result[4] = student
+            valid_options.push(result)
+          end
+          seen_students[student] = "yes"
+          result[2].each do |infected_student|
+            seen_students[infected_student] = "yes"
+          end
+
+
+
+
+
+        end
+
+      end
+        p 77
+      if (valid_options.length === 0)
+        return "Limited Infection Not Possible"
+      else
+        return valid_options
       end
     end
-      p 77
-    if (valid_options.length === 0)
-      return "Limited Infection Not Possible"
-    else
-      return valid_options
-    end
-  end
 end
